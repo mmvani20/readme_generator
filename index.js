34aba6ae682bdb027f9c7ad87e56954de50e1037
mosses vani <mmvani20@gmail.com>
2021-01-20 18:43:49 -0500

    working process readme generator

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const path = require("path");
 const inquirer = require("inquire");
 const generateMarkdown = require("./utils/generateMarkdown");
+const util = require("util")
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -14,23 +15,31 @@ const questions = [
     {
         type: "input",
         name: "github",
-        message: "What is your github userName?",
+        message: "What is your email address?",
     },
 
     {
         type: "input",
         name: "github",
-        message: "What is your github userName?",
+        message: "What is your facebook?",
     },
     {
         type: "input",
         name: "github",
-        message: "What is your github userName?",
+        message: "What is your linkin profile?",
     }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+          return console.log(err);
+        }
+      
+        console.log("Success! Your README.md file has been generated")
+    });
+}
 
 // TODO: Create a function to initialize app
 function init() {
